a8a1ca51b70c959c0aedccff29d594b3626a5dda
jllahi <1439075+jllahi@users.noreply.github.com>
2024-04-18 21:28:57 +0200

    Update presentation tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -17,7 +17,6 @@ if (!projectId || !dataset) {
 
 const previewUrl = import.meta.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:4321'
 
-import { locate } from '@/utils/locate'
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { media } from 'sanity-plugin-media'
@@ -34,7 +33,7 @@ export default defineConfig({
 		presentationTool({
 			// Required: set the base URL to the preview location in the front end
 			previewUrl: previewUrl,
-			locate,
+			// locate,
 		}),
 		structureTool(),
 		media(),
